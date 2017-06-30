7f751464bbe0307f22d26a2afa653f3a91420832
fatelancer <fatelancer@icloud.com>
2017-06-30 14:04:38 +0800

    Site updated: 2017-06-30 14:04:37

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://fatelancer.github.io/ins-min/' + data.link[i] + '.min.jpg';
-          var src = 'http://fatelancer.github.io/ins/' + data.link[i];
+          var minSrc = 'http://oscjkkd1u.bkt.clouddn.com/' + data.link[i] + '.min.jpg';
+          var src = 'http://oscjkkd1u.bkt.clouddn.com/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
