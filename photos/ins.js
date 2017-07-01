1ae9e3534a7e7a5ae23d3e528a485e9cc2f3e1da
fatelancer <fatelancer@icloud.com>
2017-07-01 21:38:48 +0800

    Site updated: 2017-07-01 21:38:47

--- photos/ins.js
+++ photos/ins.js
@@ -117,7 +117,6 @@
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
           var minSrc = 'https://raw.githubusercontent.com/fatelancer/blogimage/master/min_photos/' + data.link[i] ;
-		  console.log(data.link[i])
           var src = 'https://raw.githubusercontent.com/fatelancer/blogimage/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
